b42b940a1eab012d014ed38aa7246afef496c1b8
Jordan <jordan.stakenas@enosix.com>
2021-03-14 14:23:22 -0600

    auth changes for mongoDB

--- Users.js
+++ Users.js
@@ -4,38 +4,44 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true });
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
 mongoose.set('useCreateIndex', true);
 
 //user schema
 var UserSchema = new Schema({
     name: String,
-    username: {type: String, required: true, index: { unique: true}},
-    password: {type: String, required: true, select: false }
+    username: { type: String, required: true, index: { unique: true }},
+    password: { type: String, required: true, select: false }
 });
 
-UserSchema.pre('save', function(next){
+UserSchema.pre('save', function(next) {
     var user = this;
 
-    //hash pw
+    //hash the password
     if (!user.isModified('password')) return next();
 
-    bcrypt.hash(user.password, null, null, function (err, hash){
-      if (err) return next(err);
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
 
-      //change pw
-      user.password = hash;
-      next();
+        //change the password
+        user.password = hash;
+        next();
     });
 });
 
 UserSchema.methods.comparePassword = function (password, callback) {
     var user = this;
 
-    bcrypt.compare(password, user.password, function (err, isMatch){
+    bcrypt.compare(password, user.password, function(err, isMatch) {
         callback(isMatch);
     })
 }
 
-//return model to server
-module.exports = mongoose.model('user', UserSchema);
+//return the model to server
+module.exports = mongoose.model('User', UserSchema);
